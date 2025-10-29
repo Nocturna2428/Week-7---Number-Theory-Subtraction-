be9974b76ed129c4fbd59a53c2800cc576ad7fb4
Nocturna2428 <162833266+Nocturna2428@users.noreply.github.com>
2025-10-29 11:20:37 -0600

    Refactor module instantiation and naming conventions

--- top.v
+++ top.v
@@ -4,23 +4,24 @@ output [13:0]led
 );
 
 
-half_sub u1(
+half_sub hsub_inst(
 .A(sw[0]),
 .B(sw[1]),
 .Y(led[0]),
 .Borrow(led[1])
 );
 
-ones_compliment onecomp (
+ones_compliment ones_comp_inst (
 .A(sw[5:2]), .B(sw[9:6]),
-.Sum(led[5:2])
+.Y(led[5:2])
 );
 
 // Second full adder (MSB + carry from LSB)
-twos_compliment twocomp(
+twos_compliment twos_comp_inst(
 .num(sw[9:2]),
 .sum(led[13:6])
 );
 
 
 endmodule
+
