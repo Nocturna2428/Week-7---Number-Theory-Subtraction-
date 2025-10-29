210eac6408cb114f591f9d35520141749e94fedb
Nocturna2428 <162833266+Nocturna2428@users.noreply.github.com>
2025-10-29 11:27:56 -0600

    Update twos_compliment.v

--- twos_compliment.v
+++ twos_compliment.v
@@ -1,86 +1,82 @@
 module twos_compliment(
-input [7:0] i
+  input [7:0] A,
+  output [7:0]Y
 //output [7:0] o, [7:0] Y
 );
 
-wire [7:0] o, [7:0] Y;
+  wire inversion[7:0];
 
-assign o[0] = -i[0];
-assign o[1] = -i[1];
-assign o[2] = -i[2];
-assign o[3] = -i[3];
-assign o[4] = -i[4];
-assign o[5] = -i[5];
-assign o[6] = -i[6];
-assign o[7] = -i[7];
+assign inversion[0] = -i[0];
+assign inversion[1] = ~A[1];
+assign inversion[2] = ~A[2];
+assign inversion[3] = ~A[3];
+assign inversion[4] = ~A[4];
+assign inversion[5] = ~A[5];
+assign inversion[6] = ~A[6];
+assign inversion[7] = ~A[7];
 
 
 wire [6:0] carry;
 
 //assign notNum = ~num; // Invert bits
 
-full_adder fa0_inst (
-.A(o[0]),
+full_adder bit0 (
+  .A(inversion[0]),
 .B(1),
-.C(0),
-.Y(Y[0]),
-.cout(carry[0])
+.Cin(0),
+.Y(Y[0])
 );
 
-full_adder fa1_inst (
-.A(o[1]),
+full_adder bit1 (
+.A(inversion[1]),
 .B(0),
-.C(carry[0]),
+  .Cin(bit0.Cout),
 .Y(Y[1]),
-.cout(carry[1])
+
 );
 
-full_adder fa2_inst (
-.A(o[2]),
+full_adder bit2 (
+.A(inversion[2]),
 .B(0),
-.C(carry[1]),
+.Cin(bit1.Cout),
 .Y(Y[2]),
-.cout(carry[2])
+
 );
 
-full_adder fa3_inst (
-.A(o[3]),
+full_adder bit3 (
+ .A(inversion[3]),
+ .B(0),
+ .Cin(bit2.Cout),
+ .Y(Y[3])
+);
+
+full_adder bit4 (
+ .A(inversion[4]),
+ .B(0),
+ .Cin(bit3.Cout),
+ .Y(Y[4])
+);
+
+full_adder bit5 (
+ .A(inversion[5]),
+ .B(0),
+ .Cin(bit4.Cout),
+ .Y(Y[5])
+);
+
+full_adder bit6 (
+.A(inversion[6]),
 .B(0),
-.C(carry[2]),
-.Y(Y[3]),
-.cout(carry[3])
+.Cin(bit5.Cout),
+.Y(Y[6])
 );
 
-full_adder fa4_inst (
-.A(o[4]),
-.B(1),
-.C(carry[3]),
-.Y(Y[4]),
-.cout(carry[4])
-);
-
-full_adder fa5_inst (
-.A(o[5]),
+full_adder bit7 (
+.A(inversion[7]),
 .B(0),
-.C(carry[4]),
-.Y(Y[5]),
-.cout(carry[5])
+.Cin(bit6.Cout),
+.Y(Y[7])
 );
 
-full_adder fa6_inst (
-.A(o[6]),
-.B(0),
-.C(carry[5]),
-.Y(Y[6]),
-.cout(carry[6])
-);
-
-full_adder fa7_inst (
-.A(o[7]),
-.B(0),
-.C(carry[6]),
-.Y(Y[07]),
-.count(carry[0])
-);
 
 endmodule
